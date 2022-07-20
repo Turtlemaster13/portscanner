4ad206e29f4644cebbd30741b412959feb41581b
Turtlemaster13 <104543638+Turtlemaster13@users.noreply.github.com>
2022-07-19 20:56:10 -0700

    tested nmap

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 #the file I test stuff in
 import nmap
+import sys
+
+args = sys.argv
+
+host = args[1]
+protocol = args[2]
 np = nmap.PortScanner()
-print(np.scan("localhost", "22-443"))
+#print(np.scan("localhost", "1-443"))
+np.scan(host, "1-65535")
+for key in np[host]['tcp'].keys():
+    #print(key)
+    #print ('port : %s\tstate : %s' % (key, np[host][protocol][key]['state']))
+    print("port :",key, "\tstate :", np[host][protocol][key]['state'])
