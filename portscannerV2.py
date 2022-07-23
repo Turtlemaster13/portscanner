7076d2f222d60881be6ad893066d334f15e977de
Turtlemaster13 <104543638+Turtlemaster13@users.noreply.github.com>
2022-07-22 20:42:17 -0700

    time stuff

--- portscannerV2.py
+++ portscannerV2.py
@@ -1,10 +1,14 @@
 
+from unittest import skip
 import nmap
 import sys
 import json
 from datetime import datetime
 
+
 args = sys.argv# get the arguments
+runscan = True
+
 
 try:# if there is a first argument
     host = args[1]# then set the host to the argument
@@ -18,14 +22,32 @@ except:
     port_range_end = 65535
 protocol = "tcp"#set the protocal to tcp
 
+Datetime = datetime.now()# get the datetime
+
+with open("data.json", "r") as file:
+    data = json.load(file)
+    time_gap_hours = (datetime.timestamp(Datetime) - int(data['timestamp']) ) // 3600
+    print(time_gap_hours)
+    if time_gap_hours <= 2:
+        choice = input("you have a scan from within the last 2 hours, would you like to use that? y/n ")
+        if choice == "y":
+            for p in data:
+                if str(p) != "time" and str(p) != "timestamp":
+                    print("port : " ,p, "\t state : ", data[p]["state"], "\t name : ", data[p]["name"])
+            file.close()
+            sys.exit(0)
+
+print(f'staring scan of ports {port_range_start} through {port_range_end} on {host}')
+
+
 np = nmap.PortScanner()#create the port scanner object
 np.scan(host, str(port_range_start) + "-" + str(port_range_end))#scan the ports from range port_range_start to port_range_end
 
 closedports = 0#set closed ports to 0
 data = {}#intialize data dictionary
 
-Datetime = datetime.now()# get the datetime
 data["time"] = datetime.strftime(Datetime,"%m/%j/%y %H:%M")#convert the date time to a more readable format
+data["timestamp"] = datetime.timestamp(Datetime)
 
 for i in range(int(port_range_start) -1, int(port_range_end)):#for every port in the computer
     try:#print that the port is open, if that fails the port is closed, otherise put it in a dictionary to be put in the file
